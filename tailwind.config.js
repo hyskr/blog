24dc3609f9e1f32bd4644c844e2534d68277bd00
Cosmic <65348517+hyskr@users.noreply.github.com>
2023-08-21 18:02:23 +0800

    添加配置文件和其他杂项

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        "./.vitepress/theme/**/*.{vue,js,ts}",
+    ],
+    theme: {
+        extend: {},
+    },
+    screens: {
+        'sm': '640px',
+    },
+    plugins: [],
+}
